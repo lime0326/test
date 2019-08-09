c825d02dc1cf5ab4699f91667216f5604bca725f
lime0326 <33085507+lime0326@users.noreply.github.com>
2019-08-09 19:30:00 +0900

    Create index.php

---
+++ index.php
@@ -0,0 +1,5 @@
+<div class="bb_btns">
+    <a href="#" class='btn btn-secondary'>수정</a>
+    <a href="#" class='btn btn-secondary'>삭제</a>
+    <a href="./?page=<?=$page?>" class='btn btn-secondary'>목록</a>
+</div>
